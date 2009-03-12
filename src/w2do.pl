edb9548d7513806d54e57fc63e1a92afbc677765
Jaromir Hradilek <jhradilek@gmail.com>
2009-03-13 00:59:28 +0100

    Added Pod (Plain Old Documentation) to w2text.
    The documentation is now part of the source file itself instead of an
    external roff file. I have also corrected the FDL version notice in w2do.

--- src/w2do.pl
+++ src/w2do.pl
@@ -1309,7 +1309,7 @@ contact the author directly via e-mail.
 Written by Jaromir Hradilek <jhradilek@gmail.com>.
 
 Permission is granted to copy, distribute and/or modify this document under
-the terms of the GNU Free Documentation License, Version 1.2 or any later
+the terms of the GNU Free Documentation License, Version 1.3 or any later
 version published by the Free Software Foundation; with no Invariant
 Sections, no Front-Cover Texts, and no Back-Cover Texts.
 
